feb82b744db48e2c6d05c2e45bac21aa2ad12c00
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-05-15 18:17:36 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/SignVision/Sign-Vision into main""
    This reverts commit e6ec40b325aea62d78bce886d9affac77ef6c54e.

--- app/src/main/java/com/example/signvision/SpeechToSign.java
+++ app/src/main/java/com/example/signvision/SpeechToSign.java
@@ -2,6 +2,7 @@ package com.example.signvision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,13 @@ public class SpeechToSign extends AppCompatActivity {
         setContentView(R.layout.activity_speech_to_sign);
     }
     public void onTranslateClicked(View view) {
+
+
+    }
+
+    public void BacktoHomePage(View view) {
+        Intent SpeechToSignIntent = new Intent(SpeechToSign.this, HomeActivity.class);
+        startActivity(SpeechToSignIntent);
 
 
     }
