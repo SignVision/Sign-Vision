7a89ed373116144994d3443b366a74b41e1538ed
Jeannie Q <jeanniequach01@gmail.com>
2021-05-15 16:23:43 -0400

    Changing names
    + setting up connectivity

--- app/src/main/java/com/example/signvision/MainActivity.java
+++ app/src/main/java/com/example/signvision/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
+                Intent howToUseIntent = new Intent(MainActivity.this, HowToUse.class);
+                startActivity(howToUseIntent);
                 finish();
             }
         }, SPLASH_TIME_OUT);
