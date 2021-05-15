df67a9630a4334795a843c5b8acf013b574dc5b4
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-05-15 19:52:30 -0400

    added sign pics

---
+++ app/src/main/java/com/example/signvision/SignLanguage.java
@@ -0,0 +1,5 @@
+package com.example.signvision;
+
+public class SignLanguage {
+
+}
--- app/src/main/java/com/example/signvision/TextToSign.java
+++ app/src/main/java/com/example/signvision/TextToSign.java
@@ -3,6 +3,8 @@ package com.example.signvision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class TextToSign extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class TextToSign extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_to_sign);
+    }
+    public void onTranslateClicked(View view) {
+        EditText text = (EditText)findViewById(R.id.inputField);
+        String value = text.getText().toString();
+
+
     }
 }
