0a6422a1d3e2a8e192804e5cf402765657780a81
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-05-15 17:58:03 -0400

    d

--- app/src/main/java/com/example/signvision/SpeechToSign.java
+++ app/src/main/java/com/example/signvision/SpeechToSign.java
@@ -3,12 +3,17 @@ package com.example.signvision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SpeechToSign extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_speech_to_text);
+        setContentView(R.layout.activity_speech_to_sign);
+    }
+    public void onTranslateClicked(View view) {
+
+
     }
 }
