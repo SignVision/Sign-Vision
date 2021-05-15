ed5beddf1d325269b494705e64ff149667ed56a5
Jeannie Q <jeanniequach01@gmail.com>
2021-05-15 15:48:21 -0400

    Finishing designs
    Missing download button + connectivity between pages

--- app/src/main/java/com/example/signvision/HowToUse.java
+++ app/src/main/java/com/example/signvision/HowToUse.java
@@ -3,6 +3,8 @@ package com.example.signvision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class HowToUse extends AppCompatActivity {
 
@@ -11,4 +13,22 @@ public class HowToUse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_use);
     }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.warning:
+                if (checked){
+
+                }
+                else {
+                    break;
+                }
+        }
+    }
 }
+
+
