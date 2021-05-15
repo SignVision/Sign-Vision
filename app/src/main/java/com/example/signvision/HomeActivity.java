1bce0c31a95b822ce138ef430fd259013b199238
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-05-15 16:54:56 -0400

    added connections

--- app/src/main/java/com/example/signvision/HomeActivity.java
+++ app/src/main/java/com/example/signvision/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.signvision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void onSignToTextClicked(View view) {
+        Intent SignToTextIntent = new Intent(HomeActivity.this, SignLanguagetoText.class);
+        startActivity(SignToTextIntent);
+
+
+    }
+    public void onSpeechToSignClicked(View view) {
+        Intent SpeechToSignIntent = new Intent(HomeActivity.this, SpeechToSign.class);
+        startActivity(SpeechToSignIntent);
+
+
+    }
+    public void onTextToSignClicked(View view) {
+        Intent TextToSignIntent = new Intent(HomeActivity.this, TextToSign.class);
+        startActivity(TextToSignIntent);
+
+
     }
 }
--- app/src/main/java/com/example/signvision/TextToSign.java
+++ app/src/main/java/com/example/signvision/TextToSign.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SpeechToText extends AppCompatActivity {
+public class TextToSign extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_speech_to_text);
+        setContentView(R.layout.activity_video_to_transcript);
     }
 }
